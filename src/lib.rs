027375e93300854cdadbe62a9a798d3a1ccb6e28
Juan Tamez <tamez.jm@gmail.com>
2024-11-29 20:10:22 -0600

    asd

--- src/lib.rs
+++ src/lib.rs
@@ -1,37 +1,21 @@
-//!
-//! Stylus Hello World
-//!
-//! The following contract implements the Counter example from Foundry.
-//!
-//! ```
-//! contract Counter {
-//!     uint256 public number;
-//!     function setNumber(uint256 newNumber) public {
-//!         number = newNumber;
-//!     }
-//!     function increment() public {
-//!         number++;
-//!     }
-//! }
-//! ```
-//!
-//! The program is ABI-equivalent with Solidity, which means you can call it from both Solidity and Rust.
-//! To do this, run `cargo stylus export-abi`.
-//!
-//! Note: this code is a template-only and has not been audited.
-//!
-
-// Allow `cargo stylus export-abi` to generate a main function.
-#![cfg_attr(not(feature = "export-abi"), no_main)]
+#![cfg_attr(not(test), no_main)]
 extern crate alloc;
-
 
 use alloc::vec::Vec;
 
-use alloy_primitives::{Address, U256};
-use openzeppelin_stylus::token::erc20::{extensions::Erc20Metadata, Erc20};
+use alloy_primitives::{Address, FixedBytes, U256};
+use openzeppelin_stylus::{
+    token::erc20::{
+        extensions::{capped, Capped, Erc20Metadata, IErc20Burnable},
+        Erc20, IErc20,
+    },
+    utils::{introspection::erc165::IErc165, Pausable},
+};
 use stylus_sdk::prelude::{entrypoint, public, sol_storage};
 
+const DECIMALS: u8 = 10;
+const NAME: &str = "Erc20 Example";
+const SYMBOL: &str = "ERC20";
 sol_storage! {
     #[entrypoint]
     struct Erc20Example {
@@ -39,22 +23,106 @@ sol_storage! {
         Erc20 erc20;
         #[borrow]
         Erc20Metadata metadata;
+        #[borrow]
+        Capped capped;
+        #[borrow]
+        Pausable pausable;
     }
 }
 
 #[public]
-#[inherit(Erc20, Erc20Metadata)]
+#[inherit(Erc20, Erc20Metadata, Capped, Pausable)]
 impl Erc20Example {
+    // Overrides the default [`Metadata::decimals`], and sets it to `10`.
+    //
+    // If you don't provide this method in the `entrypoint` contract, it will
+    // default to `18`.
+    pub fn decimals(&self) -> u8 {
+        DECIMALS
+    }    
+    
+    fn name(&self) -> String {
+        NAME.to_string()
+    }    pub fn symbol(&self) -> String {
+        SYMBOL.to_string()
+    }
+
+    pub fn burn(&mut self, value: U256) -> Result<(), Vec<u8>> {
+        self.pausable.when_not_paused()?;
+        self.erc20.burn(value).map_err(|e| e.into())
+    }
+
+    pub fn burn_from(
+        &mut self,
+        account: Address,
+        value: U256,
+    ) -> Result<(), Vec<u8>> {
+        self.pausable.when_not_paused()?;
+        self.erc20.burn_from(account, value).map_err(|e| e.into())
+    }
+
+    // Add token minting feature.
+    //
+    // Make sure to handle `Capped` properly. You should not call
+    // [`Erc20::_update`] to mint tokens -- it will the break `Capped`
+    // mechanism.
     pub fn mint(
         &mut self,
         account: Address,
         value: U256,
     ) -> Result<(), Vec<u8>> {
+        self.pausable.when_not_paused()?;
+        let max_supply = self.capped.cap();
+
+        // Overflow check required.
+        let supply = self
+            .erc20
+            .total_supply()
+            .checked_add(value)
+            .expect("new supply should not exceed `U256::MAX`");
+
+        if supply > max_supply {
+            return Err(capped::Error::ExceededCap(
+                capped::ERC20ExceededCap {
+                    increased_supply: supply,
+                    cap: max_supply,
+                },
+            ))?;
+        }
+
         self.erc20._mint(account, value)?;
         Ok(())
     }
 
-    pub fn name(&self) -> String {
-        "ATON Stylus".into()
+    pub fn transfer(
+        &mut self,
+        to: Address,
+        value: U256,
+    ) -> Result<bool, Vec<u8>> {
+        self.pausable.when_not_paused()?;
+        self.erc20.transfer(to, value).map_err(|e| e.into())
+    }
+
+    pub fn transfer_from(
+        &mut self,
+        from: Address,
+        to: Address,
+        value: U256,
+    ) -> Result<bool, Vec<u8>> {
+        self.pausable.when_not_paused()?;
+        self.erc20.transfer_from(from, to, value).map_err(|e| e.into())
+    }
+
+    fn supports_interface(interface_id: FixedBytes<4>) -> bool {
+        Erc20::supports_interface(interface_id)
+            || Erc20Metadata::supports_interface(interface_id)
+    }
+
+    pub fn pause(&mut self) -> Result<(), Vec<u8>> {
+        self.pausable.pause().map_err(|e| e.into())
+    }
+
+    pub fn unpause(&mut self) -> Result<(), Vec<u8>> {
+        self.pausable.unpause().map_err(|e| e.into())
     }
 }
