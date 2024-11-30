af6da293c3e27f9e0cae9cf962a6f827d5ac0654
Juan Tamez <tamez.jm@gmail.com>
2024-11-29 19:54:51 -0600

    asd

--- src/lib.rs
+++ src/lib.rs
@@ -25,17 +25,36 @@
 #![cfg_attr(not(feature = "export-abi"), no_main)]
 extern crate alloc;
 
-/// Import items from the SDK. The prelude contains common traits and macros.
-use stylus_sdk::{alloy_primitives::U256, prelude::*};use openzeppelin_stylus::token::erc20::Erc20;
+
+use alloc::vec::Vec;
+
+use alloy_primitives::{Address, U256};
+use openzeppelin_stylus::token::erc20::{extensions::Erc20Metadata, Erc20};
+use stylus_sdk::prelude::{entrypoint, public, sol_storage};
 
 sol_storage! {
     #[entrypoint]
     struct Erc20Example {
         #[borrow]
         Erc20 erc20;
+        #[borrow]
+        Erc20Metadata metadata;
     }
 }
 
 #[public]
-#[inherit(Erc20)]
-impl Erc20Example {}
+#[inherit(Erc20, Erc20Metadata)]
+impl Erc20Example {
+    pub fn mint(
+        &mut self,
+        account: Address,
+        value: U256,
+    ) -> Result<(), Vec<u8>> {
+        self.erc20._mint(account, value)?;
+        Ok(())
+    }
+
+    pub fn name(&self) -> String {
+        "ATON Stylus".into()
+    }
+}
